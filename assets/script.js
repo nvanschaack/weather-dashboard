8f31d2fb89e41086218c0786d49668cb2e3ee77d
nvanschaack <nellvanschaack@gmail.com>
2024-04-15 09:59:24 -0500

    added for loop to forecast to show forecast for the next 5 days

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,7 @@
 const submit = document.getElementById('form')
 const searchBar = document.getElementById('search-bar')
 const currentWeather = document.getElementById('current-weather')
+const forecast = document.getElementById('forecast')
 
 submit.addEventListener('submit', function (event) {
     event.preventDefault();
@@ -10,6 +11,8 @@ submit.addEventListener('submit', function (event) {
     const searchInput = searchBar.value
     // localStorage.setItem('searchInput', searchInput)
     //need to getItem from localStorage
+    currentWeather.innerHTML = null
+    forecast.innerHTML = null
 
     getCurrentWeatherApi(searchInput)
     getForecaseWeatherApi(searchInput)
@@ -25,7 +28,7 @@ function getCurrentWeatherApi(city) {
             return response.json()
         })
         .then(function (data) {
-            console.log('current weather',data)
+            console.log('current weather', data)
             // with data, i want to present it in current weather div
             //create element(s) to put city name, date, weather icon, temperature, humidity, and wind
             const cityName = document.createElement('h2')
@@ -38,7 +41,7 @@ function getCurrentWeatherApi(city) {
             const icon = document.createElement('img')
             icon.setAttribute('src', `http://openweathermap.org/img/w/${data.weather[0].icon}.png`)
 
-            cityName.textContent = data.name + ' '+timeString
+            cityName.textContent = data.name + ' ' + timeString
             temperature.textContent = `The current temperature is ${data.main.temp}℉`
             humidity.textContent = `The humidity is ${data.main.humidity}%`
             wind.textContent = `The wind speed is ${data.wind.speed}mph`
@@ -59,31 +62,27 @@ function getForecaseWeatherApi(city) {
             return response.json()
         })
         .then(function (data) {
-            console.log('forecast',data)
-
-            // with data, i want to present it in forecast div
-            //create element(s) to put date, temperature, humidity, and wind
-
-            const temperature = document.createElement('p')
-            const humidity = document.createElement('p')
-            const wind = document.createElement('p')
-            const unixTimestamp = data.list[0].dt;
-            const date = new Date(unixTimestamp * 1000);
-            const timeString = date.toLocaleDateString();
-            const icon = document.createElement('img')
-            icon.setAttribute('src', `http://openweathermap.org/img/w/${data.list[0].weather[0].icon}.png`)
-
-            temperature.textContent = `The current temperature is ${data.list[0].main.temp}℉`
-            humidity.textContent = `The humidity is ${data.list[0].main.humidity}%`
-            wind.textContent = `The wind speed is ${data.list[0].wind.speed}mph`
-
-            forecast.append(timeString, icon, temperature, humidity, wind)
+            console.log('forecast', data)
 
             // i want to make a loop so that the weather dashboard will display the 5-day forecast (at noon?)
             for (let i = 0; i < data.list.length; i++) {
-                const element = [i];
-                if (data.list[i] === 3 && 11 && 19 && 27 && 35) {
+                const element = data.list[i];
+                if (element.dt_txt.split(' ')[1] === '12:00:00' && i > 0) {
                     console.log(element)
+                    const temperature = document.createElement('p')
+                    const humidity = document.createElement('p')
+                    const wind = document.createElement('p')
+                    const unixTimestamp = element.dt;
+                    const date = new Date(unixTimestamp * 1000);
+                    const timeString = date.toLocaleDateString();
+                    const icon = document.createElement('img')
+                    icon.setAttribute('src', `http://openweathermap.org/img/w/${element.weather[0].icon}.png`)
+
+                    temperature.textContent = `The current temperature is ${element.main.temp}℉`
+                    humidity.textContent = `The humidity is ${element.main.humidity}%`
+                    wind.textContent = `The wind speed is ${element.wind.speed}mph`
+
+                    forecast.append(timeString, icon, temperature, humidity, wind)
                 }
             }
         })
